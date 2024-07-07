18cf5f242f5be4e4fa7c4263ed7c0a18cff11382
agennttrex <agenttrex6@gmail.com>
2024-07-08 01:26:02 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
- // JavaScript for carousel functionality
+ //carousel
  const carouselContent = document.querySelector('.carousel-content');
  const carouselSlides = document.querySelectorAll('.carousel-slide');
  const carouselTotalSlides = carouselSlides.length;
@@ -16,25 +16,27 @@
      carouselContent.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
  }
 
- // Auto slide
+ //Auto slide
  setInterval(() => {
      showSlide(currentSlide + 1);
  }, 3000);
 
+ //toggle
  const themeToggle = document.getElementById('theme-toggle');
 
 themeToggle.addEventListener('click', () => {
-document.body.classList.toggle('dark-theme'); // Toggle dark theme class
+document.body.classList.toggle('dark-theme');
 });
 
+//Animations
 gsap.from("#H", {
-x: "-100%", // Start position: move from left (off-screen)
-duration: 1.5, // Animation duration
-ease: "power2.out" // Easing function (optional)
+x: "-100%",
+duration: 1.5,
+ease: "power2.out"
 });
 
 gsap.from("#h", {
-x: "100%", // Start position: move from left (off-screen)
-duration: 1.5, // Animation duration
-ease: "power2.out" // Easing function (optional)
+x: "100%",
+duration: 1.5,
+ease: "power2.out"
 });
